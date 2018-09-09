78518a20fde0e99fb2e99178e67266edf4b00200
Cristopher Jimenez <crisjimenez120@gmail.com>
2018-09-09 09:20:18 -0400

    added register validation

--- Resolvers/user.js
+++ Resolvers/user.js
@@ -1,4 +1,14 @@
 import bcrypt from 'bcrypt';
+import _ from 'lodash';
+
+const formatErrors = (e, models) => {
+  if (e instanceof models.sequelize.ValidationError) {
+    return e.errors.map(x => _.pick(x, ['path', 'message']));
+  }
+  return [{ path: 'name', message: 'something went wrong' }];
+};
+
+
 
 export default {
   Query: {
@@ -8,12 +18,30 @@ export default {
   Mutation: {
     register: async (parent, {password, ... otherArgs}, { models }) => {
     	try{
+        if (password.length < 8 || password.length > 20) {
+          return {
+            ok: false,
+            errors: [
+              {
+                path: 'password',
+                message: 'The password needs to be between 8 and 20 characters long',
+              },
+            ],
+          };
+        }
     	const hashedPassword = await bcrypt.hash(password, 10);
-    	await models.User.create({ ... otherArgs, password: hashedPassword})
-    	return true;
+    	const user = await models.User.create({ ... otherArgs, password: hashedPassword})
+    	return {
+        ok : true,
+        user,
+
+      };
     	} catch(err){
-    		return false;
-    	}
+    		return {
+          ok: false,
+          errors: formatErrors(err, models),
+    	};
     }
+   },
   },
 };
--- models/user.js
+++ models/user.js
@@ -3,15 +3,32 @@ export default  (sequelize, DataTypes) => {
     username : {
       type : DataTypes.STRING,
       unique : true,
+      validate: {
+        isAlphanumeric: {
+          args: true,
+          msg: 'The username can only contain letters and numbers',
+
+        },
+        len:{
+          args: [3, 20],
+          msg: 'Must be between 3 and 20 characters long',
+        },
+      },
     },
     email : {
       type : DataTypes.STRING,
       unique : true,
+       validate: {
+        isEmail: {
+          args: true,
+          msg: 'Invalid Email',
+        },
+      },
     },
-    password : DataTypes.STRING,
-      
-  }, 
-  );
+    password : {
+      type : DataTypes.STRING,    
+    }, 
+  });
 
   User.associate = function (models) {
     User.belongsToMany(models.Channel, {
